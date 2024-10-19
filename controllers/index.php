bdc4e6666b8d5573a3efd6e8f4e9e7111fcef065
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2024-10-19 19:58:01 +0300

    Update controllers/index.php

--- controllers/index.php
+++ controllers/index.php
@@ -2,81 +2,38 @@
 
 $heading = "home";
 
-// Step 1: Authentication Request (POST)
-$curl = curl_init();
-curl_setopt_array($curl, [
-    CURLOPT_URL => "https://api.baubuddy.de/index.php/login",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => json_encode([
-        "username" => "365",
-        "password" => "1"
-    ]),
-    CURLOPT_HTTPHEADER => [
-        "Authorization: Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
-        "Content-Type: application/json"
-    ],
-]);
-
-$auth_response = curl_exec($curl);
-$auth_err = curl_error($curl);
-curl_close($curl);
-
-if ($auth_err) {
-    echo "Authentication cURL Error #:" . $auth_err;
-    exit;
+function searchTasks($searchTerm, $data) {
+    // Filter results based on the search term
+    return array_filter($data, function($item) use ($searchTerm) {
+        $searchTerm = strtolower($searchTerm); // Make search case-insensitive
+        return (
+            strpos(strtolower($item['task']), $searchTerm) !== false ||
+            strpos(strtolower($item['title']), $searchTerm) !== false ||
+            strpos(strtolower($item['description']), $searchTerm) !== false ||
+            strpos(strtolower($item['colorCode']), $searchTerm) !== false
+        );
+    });
 }
 
-// Decode the authentication response to get the access token
-$auth_data = json_decode($auth_response, true);
 
-if (isset($auth_data["oauth"]["access_token"])) {
-    $token = $auth_data["oauth"]["access_token"];
-} else {
-    echo "Failed to retrieve access token.";
-    exit;
-}
+// function filter($datas, $fn)
+// {
+//     $filteredData = [];
 
-// Step 2: Data Request (GET) using the token
-$curl = curl_init();
-curl_setopt_array($curl, [
-    CURLOPT_URL => "https://api.baubuddy.de/dev/index.php/v1/tasks/select",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => [
-        "Authorization: Bearer " . $token,
-        "Content-Type: application/json"
-    ],
-]);
+//     foreach($datas as  $data) {
+//         if ($fn($data)) {
+//             $filteredData[] = $data;
+//         }
+//     }
 
-$data_response = curl_exec($curl);
-$data_err = curl_error($curl);
-curl_close($curl);
+//     return $filteredData;
+// }
 
-function filter($datas, $fn)
-{
-    $filteredData = [];
+// $filtered = filter($decoded_data,  function ($data) {
+//     return $data["BusinessUnitKey"] == null;
+// });
 
-    foreach($datas as  $data) {
-        if ($fn($data)) {
-            $filteredData[] = $data;
-        }
-    }
 
-    return $filteredData;
-}
-
-$decoded_data = json_decode($data_response, true);
-$filtered = filter($decoded_data,  function ($data) {
-    return $data["BusinessUnitKey"] == null;
-});
+$decoded_data = fetchAndSaveData();
 
 require "views/index.view.php";
